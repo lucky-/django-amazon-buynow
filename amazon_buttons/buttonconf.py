439463b2b5154933b81e893afd8fcf9bb9ccb2e6
LL <head@head-1215B-1215B.(none)>
2013-06-22 13:42:30 -0500

    button form v1
    untested version of the button form

---
+++ amazon_buttons/buttonconf.py
@@ -0,0 +1,7 @@
+
+
+DEFAULT_DATA = dict(signatureMethod='HmacSHA256', signatureVersion='2')
+AMAZON_SANDBOX_URL = 'https://payments- sandbox.amazon.com/checkout/'
+AMAZON_URL = 'https://authorize.payments.amazon.com/pba/paypipeline'
+BUTTONURL ='https://authorize.payments.amazon.com/pba/images/SLPayNowWithLogo.png'
+
