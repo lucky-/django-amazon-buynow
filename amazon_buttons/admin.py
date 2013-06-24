7dc7babe7a75865ac86debea122ac76a88a219a2
LL <head@head-1215B-1215B.(none)>
2013-06-24 17:55:09 -0500

    basic ipn

---
+++ amazon_buttons/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from amazon_buttons import models
+
+admin.site.register(models.ipn_response)
--- amazon_buttons/models.py
+++ amazon_buttons/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # IPN Response
 
 class ipn_response(models.Model):
+	status = models.CharField(max_length = 100)
+	paymentReason = models.CharField(max_length = 300)
+	operation = models.CharField(max_length=200)
 	datetime =  models.DateTimeField('date published')
 	buyerEmail = models.EmailField(max_length=200)
 	recipientEmail = models.EmailField(max_length=200)
@@ -12,6 +15,8 @@ class ipn_response(models.Model):
 	transactionId = models.CharField(max_length=300)
 	paymentMethod = models.CharField(max_length=50)
 	transactionAmount = models.CharField(max_length=50)
+	def __unicode__(self):  
+        	return self.status + ' ' + self.datetime.strftime("%b %d %Y %H:%M")
 		
 
 
