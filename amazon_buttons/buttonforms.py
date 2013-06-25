d883105395e2920c27349ab2a4d6b928402f1c58
LL <head@head-1215B-1215B.(none)>
2013-06-25 16:56:28 -0500

    changes to crypt structure and testing ipnver

--- amazon_buttons/buttonforms.py
+++ amazon_buttons/buttonforms.py
@@ -7,27 +7,9 @@ import hashlib
 from django.utils.safestring import mark_safe
 import urllib
 from django.core.urlresolvers import reverse
+from amazon_buttons import _crypt
 
 
-#encryption alg for button signature. for POST only
-def sig_maker(key, outer_dict):
-	
-	in_crypt = 'POST\n'
-	data_dict = outer_dict.copy()
-	data_dict['target_url'] = data_dict['target_url'][data_dict['target_url'].find('//')+2:]
-	in_crypt +=data_dict['target_url'].lower()[:data_dict['target_url'].find('.com')+4] +'\n'
-	in_crypt += data_dict['target_url'].lower()[data_dict['target_url'].find('.com')+4:] +'\n'
-	all_hidden = data_dict.items()
-	all_hidden.remove(('target_url',data_dict['target_url']))
-	all_hidden.sort(key=lambda x:str(x[0]))
-	first = True	
-	for item in all_hidden:
-		if first:
-			in_crypt += urllib.quote(item[0]).replace('/','%2F') + '=' + urllib.quote(item[1]).replace('/','%2F')
-			first = False
-		else:
-			in_crypt += '&' + urllib.quote(item[0]).replace('/','%2F') + '=' + urllib.quote(item[1]).replace('/','%2F')
-	return base64.encodestring(hmac.new(key, in_crypt, hashlib.sha256).digest()).strip()
 
 
 
@@ -52,7 +34,7 @@ class button:
 			prepd_data['target_url'] = buttonconf.AMAZON_URL
 		if signed:
 			s_key = settings.AMAZON_SECRET_KEY
-			prepd_data['signature'] = sig_maker(s_key, prepd_data)
+			prepd_data['signature'] = _crypt.sig_maker(s_key, prepd_data,'POST')
 		form ='<form action="{0}" method="post">'.format(prepd_data['target_url'])
 		del prepd_data['target_url']
 		for name, val in prepd_data.iteritems():
